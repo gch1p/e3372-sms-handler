92ad3c75765b23f238c4254855eae7a76c2f2770
Evgeny Zinoviev <me@ch1p.io>
2021-03-23 00:38:25 +0300

    e3372: initial xml parsing

--- e3372.py
+++ e3372.py
@@ -8,10 +8,24 @@ class E3372:
         pass
 
     def auth(self):
-        soup = self._request("webserver/SesTokInfo")
-        print(soup)
+        response = self.request("webserver/SesTokInfo")
 
-    def _request(self, endpoint: str, method='GET'):
-        url = f"http://{self.ip}/api/{endpoint}"
+        cookie = response.find('SesInfo').get_text()
+        token = response.find('TokInfo').get_text()
+
+        self.headers['Cookie'] = cookie
+        self.headers['__RequestVerificationToken'] = token
+        self.headers['Content-Type'] = 'text/xml'
+
+    def device_information(self):
+        response = self.request('device/information')
+        print(response)
+
+    def device_signal(self):
+        response = self.request('device/signal')
+        print(response)
+
+    def request(self, endpoint: str, method='GET'):
+        url = f'http://{self.ip}/api/{endpoint}'
         r = requests.get(url) if method == 'GET' else requests.post(url)
-        return BeautifulSoup(r.text, "lxml")
+        return BeautifulSoup(r.text, 'xml').find('response')
